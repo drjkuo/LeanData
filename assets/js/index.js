eca1af706af41a5d6c0f400422f0e76845b93345
KUO <drjkuo@gmail.com>
2017-07-13 23:42:59 -0700

    Panel shakes when height changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
+var folderPanelHeight = 300;
+
 $("#infoToggler").click(function() {
-    $(this).find('img').toggle();
+  // $(this).find('img').toggle();
+  $(".arrow").toggle();
+  $("#news").toggle();
+  $("#panel").height($("#panel").height() === folderPanelHeight ? 1350 : folderPanelHeight);
+  // $("#panel").height( 300 );
+  // $( "#panel" ).animate({
+  //   height: ($( "#panel" ).height() === 1350) ? 300 : 1350
+  // }, 200);
+
+
 });
