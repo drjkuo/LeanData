39fd8a7de9cc464c67bfc007a0f9af83722c1dbc
KUO <drjkuo@gmail.com>
2017-07-17 02:32:57 -0700

    in docs

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -3,5 +3,12 @@ var folderPanelHeight = 300;
 $("#infoToggler").click(function() {
   $(".arrow").toggle();
   $("#news").toggle();
-  $("#panel").height($("#panel").height() === folderPanelHeight ? 1250 : folderPanelHeight);
+  console.log($("#panel").height());
+  if (Math.round($("#panel").height()) === folderPanelHeight) {
+    $("#panel").height(1250);
+  }
+  else {
+    $("#panel").height(folderPanelHeight);
+  }
+
 });
