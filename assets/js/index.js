2db1569dc200f10f519a08b57bad11f837c1c467
KUO <drjkuo@gmail.com>
2017-07-17 01:22:58 -0700

    clean

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,7 @@
 var folderPanelHeight = 300;
 
 $("#infoToggler").click(function() {
-  // $(this).find('img').toggle();
   $(".arrow").toggle();
   $("#news").toggle();
   $("#panel").height($("#panel").height() === folderPanelHeight ? 1250 : folderPanelHeight);
-  // $("#panel").animate({height:'300px'}, 500);
-  // $("#panel").height( 300 );
-  // $( "#panel" ).animate({
-  //   height: ($( "#panel" ).height() === 1350) ? 300 : 1350
-  // }, 200);
-
-
 });
