a15b828b003de83ab6f8ee2a2c65b5aabd1002b2
KUO <drjkuo@gmail.com>
2017-07-17 01:14:30 -0700

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,8 @@ $("#infoToggler").click(function() {
   // $(this).find('img').toggle();
   $(".arrow").toggle();
   $("#news").toggle();
-  $("#panel").height($("#panel").height() === folderPanelHeight ? 1350 : folderPanelHeight);
+  $("#panel").height($("#panel").height() === folderPanelHeight ? 1250 : folderPanelHeight);
+  // $("#panel").animate({height:'300px'}, 500);
   // $("#panel").height( 300 );
   // $( "#panel" ).animate({
   //   height: ($( "#panel" ).height() === 1350) ? 300 : 1350
